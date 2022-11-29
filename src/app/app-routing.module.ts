a521b7e1ce37855f77e65e413a6d14d3a9fa8f4b
Mohamad J. Makki <malanbaki@transforat.com>
2022-11-29 10:10:20 +0300

    Starting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./layout/main/main.component";
+import {BoardComponent} from "./layout/board/board.component";
 
 const routes: Routes = [
+  { path:'', redirectTo:'main', pathMatch:'full' },
+  { path:'main', component:MainComponent },
+  { path:'board', component:BoardComponent }
 ];
 
 @NgModule({
